13e9d802638a359d9dce3be58d9ba68c558ae0e5
Angel Ricardo Aguilar Ponce de León <damedistillers@gmail.com>
2024-08-27 16:26:20 -0600

    ...

---
+++ EVA1_3_AUTOMOVIL/src/eva1_3_automovil/EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_automovil;
+
+/**
+ *
+ * @author Dell
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // DECLARACION DE LAS VARIABLES
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+
+        //ASIGNAR VALORES
+        marca = "Ford";
+        modelo = "Mustang";
+        year = 2022;
+        precio = 999000.1;
+
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+    }
+    
+}
