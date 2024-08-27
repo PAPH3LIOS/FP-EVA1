9750271355f7eb7886ce7cfce9a80ac9bf727e9c
Angel Ricardo Aguilar Ponce de León <damedistillers@gmail.com>
2024-08-27 16:50:39 -0600

    Add files via upload

---
+++ EVA1_4_TELEVISION/src/eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_television;
+
+/**
+ *
+ * @author Dell
+ */
+public class EVA1_4_TELEVISION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String modelo;
+        int tamanio;
+        double precio;
+        String tecnologia;
+        int frecuencia;
+        
+        marca = "Samsung";
+        modelo = "Crystal UHD";
+        tamanio = 70;
+        precio = 40000;
+        tecnologia = "LED";
+        frecuencia = 60;
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(tamanio);
+        System.out.println(precio);
+        System.out.println(tecnologia);
+        System.out.println(frecuencia);
+        
+    }
+    
+}
